274fc54968ca7e4631b9a7cf3b13259e5d7ebc6f
Santiago-Parra88 <sparrar@est.ups.edu.ec>
2024-10-11 23:55:19 -0500

    Ajuste Con docker o ejecucion normal

--- U2_TALLER_API RESTful/api_rest/config.js
+++ U2_TALLER_API RESTful/api_rest/config.js
@@ -1,6 +1,10 @@
 const config = {
+    //Validar sin Dockerizar
     PORT: 3000,
     DB_URL: 'mongodb://dba:12345@localhost:27017/miapp?authSource=admin'
+    //Dockerizado se cambia el puerto para que no de conflicto
+    /*PORT: 9191,
+    DB_URL: 'mongodb://dba:12345@mongodb:27017/miapp?authSource=admin'*/
 }
 
 module.exports = config
